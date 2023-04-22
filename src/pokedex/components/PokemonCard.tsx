ee657c240ff814d8bdc2aa709bb4f92d532ad08a
Brandon Jaimes <jaimes1br@gmail.com>
2023-04-21 23:55:04 -0600

    `style` PokemonCard

--- src/pokedex/components/PokemonCard.tsx
+++ src/pokedex/components/PokemonCard.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom"
 import { BasicPokemon } from '../../shared/types/types';
+import { pipePokemonName } from "../../helpers";
 
 interface Props {
   pokemon: BasicPokemon
@@ -9,12 +10,15 @@ export const PokemonCard = ({ pokemon }: Props ) => {
   return (
     <NavLink to="/detail/23" className="col-3 my-3 link-card" >
         <div className="d-flex py-3 px-2 flex-column justify-content-around card_pokemon-list" >
-            <h6 className="card_pokemon-list--text_id ms-auto">{pokemon.id}</h6>
+            <h6 className="card_pokemon-list--text_id ms-auto"># {pokemon.id}</h6>
             <div className="card_pokemon-list--img align-self-center">
                 <img className="img-filter" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} alt="" />
             </div>
             <div className="d-flex align-items-center card_pokemon-list--info py-2" >
-                <h4 className="card_pokemon-list--text_name free-space">{ pokemon.name }</h4>
+                <h4 
+                  className="card_pokemon-list--text_name free-space ps-2">
+                    { pipePokemonName(pokemon.name) }
+                </h4>
                 <span className="material-symbols-outlined icon_heart">favorite</span>
             </div>
         </div>
