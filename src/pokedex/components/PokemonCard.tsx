8dacbff08571fd567e98c447aac599bbcb1ff363
Brandon Jaimes <jaimes1br@gmail.com>
2023-05-11 15:33:42 -0600

    `feat` Favorites page

--- src/pokedex/components/PokemonCard.tsx
+++ src/pokedex/components/PokemonCard.tsx
@@ -12,18 +12,30 @@ export const PokemonCard = ({ pokemon }: Props ) => {
 
   return (
     <div className="col-3 my-3"> 
-      <div className="d-flex py-3 px-2 flex-column justify-content-around card_pokemon-list" >
+      <div className={ classnames(
+          'd-flex py-3 px-2 flex-column justify-content-around card_pokemon-list',
+          {'card_fav': isFav },
+        )}>
         <NavLink to={`/detail/${pokemon.id}`} className="d-flex flex-column link-card">
-          <h6 className="card_pokemon-list--text_id ms-auto"># { pokemon.id }</h6>
+          <h6 className={classnames("card_pokemon-list--text_id ms-auto", {'card_text_id--fav': isFav})}># { pokemon.id }</h6>
           <div className="card_pokemon-list--img align-self-center">
-            <img className="img-filter" 
+            <img className={ classnames(
+                "img-filter",
+                {'img-filter_fav': isFav}
+              )}
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} 
                 alt={ pokemon.name } />
           </div>
         </NavLink>
-        <div className="d-flex align-items-center card_pokemon-list--info" > 
+        <div className={ classnames(
+            'd-flex align-items-center card_pokemon-list--info',
+            {'card_list--info--fav': isFav}
+          )}> 
             <NavLink to={`/detail/${pokemon.id}`} 
-              className="card_pokemon-list--text_name  align-self-center link-card free-space p-2 mb-0">
+              className={ classnames(
+                'card_pokemon-list--text_name align-self-center link-card free-space p-2 mb-0',
+                {'card_text_name--fav': isFav }
+              )}>
                 { name }
             </NavLink>
             <span 
--- src/pokedex/pages/FavoritesPage.tsx
+++ src/pokedex/pages/FavoritesPage.tsx
@@ -1,8 +1,90 @@
+import { NavLink } from "react-router-dom"
+import { BasicPokemon } from "../../shared/types";
+import { useEffect, useState } from "react";
+import { useAppSelector } from "../../store/hooks";
+import { Loader } from "../../shared";
+import { PokemonCard } from "../components";
 
 export const FavoritesPage = () => {
+  
+  const [ listPokemonPage, setListPokemonPage ] = useState<BasicPokemon[]>([]);
+  const { allPokemons = [], favPokemons } = useAppSelector(state => state.pokemons);
+  const [ isEmptyFav, setIsEmptyFav ] = useState<boolean>(false);
+  const [ isLoadingFavs, setIsLoadingFavs] = useState<boolean>(true);
+  const [ numFav, setNumFav ] = useState<number>(0);
+
+
+  const setPokemonPage = () => {
+    setNumFav(favPokemons.length);
+    setIsEmptyFav(false);
+    const temp = favPokemons.map( fav => allPokemons[fav - 1]);
+
+    setListPokemonPage(temp);
+    setIsLoadingFavs(false)
+  }
+
+  useEffect(() => {
+    
+    if(allPokemons.length > 0 && favPokemons.length > 0) setPokemonPage();
+    if(favPokemons.length === 0) {
+      setIsEmptyFav(true);
+      setIsLoadingFavs(false);
+    }
+  },[ allPokemons, favPokemons ])
+ 
   return (
     <>
-        <h3>Hola favsss</h3>
+      { (isLoadingFavs) && <Loader/>}
+      { (!isLoadingFavs) && 
+        <>
+          <div className="container mt-4">
+            <div>
+              <h3 className="text-center">Mis pokemons favoritos</h3>
+              <hr className="hr-div"/>
+              <div className="d-flex justify-content-end align-text-top">
+                <img
+                  alt="pokeball"
+                  src="/assets/pokeball-nav.png"
+                  className="d-inline-block"
+                  width='30'
+                  height='30'
+                />
+                <h5 className="ms-2">{ numFav }</h5>
+              </div>
+            </div>
+          </div>
+            { 
+              (isEmptyFav ) && 
+                <div className="d-flex flex-column align-items-center 
+                text-center">
+                  <span className="material-symbols-outlined icon-size-sad">
+                    sentiment_dissatisfied
+                  </span>
+                  <h1 className="notPokemons my-2">Uppss...</h1>
+                  <h5 className="mt-2 mb-5">Aún no tienes pokemones favoritos.</h5>
+                  <NavLink 
+                    className='btn btn-back font-search'
+                    to={'/search?page=1'}>
+                      <span className="material-symbols-outlined align-middle me-1">
+                        search
+                      </span>
+                      Buscar
+                  </NavLink>
+                </div>
+            
+            }
+            {
+              (!isEmptyFav ) &&
+                <div className="row justify-content-center">
+                  {
+                    listPokemonPage.map((pkm,i) => (
+                      <PokemonCard key={i} pokemon={pkm}/>
+                    ))
+                  }
+                </div>
+            }
+        </>
+      }
     </>
   )
 }
